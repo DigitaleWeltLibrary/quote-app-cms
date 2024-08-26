a9f074f2c328f050c8a52a3f8c43568a11a3eeed
Raphael Bröderbauer <info@digitaleweltlibrary.at>
2024-08-26 16:07:46 +0200

    new data at the top

--- src/app/server/fetchdata.js
+++ src/app/server/fetchdata.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 export async function fetchdata() {
   try {
     const allQuotes = await prisma.data.findMany();
-    return allQuotes;
+    return allQuotes.reverse();
   } catch (error) {
     console.error("Fehler beim Abrufen der Daten:", error);
     throw error;
