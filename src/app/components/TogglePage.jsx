f28f6dcdf30b6ba2fc703d8d84377cc0e0479159
Raphael Bröderbauer <info@digitaleweltlibrary.at>
2024-08-26 16:21:30 +0200

    toggle resp with icon

--- src/app/components/TogglePage.jsx
+++ src/app/components/TogglePage.jsx
@@ -1,10 +1,18 @@
 import style from "../scss/components/togglepage.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear, faListCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function TogglePage({ site, change }) {
   return (
     <section className={site ? `${style.toggle} ${style.data}` : style.toggle}>
-      <span onClick={() => change(true)}>Daten</span>
-      <span onClick={() => change(false)}>Einstellungen</span>
+      <span onClick={() => change(true)}>
+        <FontAwesomeIcon icon={faGear} color="#7d8e95" size="xxl" />
+        <p>Daten</p>
+      </span>
+      <span onClick={() => change(false)}>
+        <FontAwesomeIcon icon={faListCheck} color="#7d8e95"  size="xxl"/>
+        <p>Einstellungen</p>
+      </span>
     </section>
   );
 }
