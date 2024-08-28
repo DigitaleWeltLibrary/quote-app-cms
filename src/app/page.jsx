12ff3591015442a1f2efdb6ff7b1a869532c5e47
Raphael Bröderbauer <info@digitaleweltlibrary.at>
2024-08-28 21:00:32 +0200

    frontend set title

---
+++ src/app/page.jsx
@@ -0,0 +1,28 @@
+"use server";
+import fs from "fs";
+import path from "path";
+import Card from "./components/card";
+import style from "./scss/pages/index.module.scss";
+import { metadata } from "./layout";
+import { title } from "process";
+
+
+export async function generateMetadata() {
+  const filePath = path.join(process.cwd(), "public", "json", "settings.json");
+  const fileContents = fs.readFileSync(filePath, "utf8");
+  const metadataJson = JSON.parse(fileContents);
+
+  return {
+    title: metadataJson.title,
+    description: metadataJson.description,
+    keywords: metadataJson.keywords,
+  };
+}
+
+export default async function Index() {
+  return (
+    <main className={style.main}>
+      <Card />
+    </main>
+  );
+}
